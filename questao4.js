84d3f0015dede5092aa12f619ea5b78d57c62cd6
Gaudencio <164903641+ValdirGaudencio@users.noreply.github.com>
2026-03-03 11:31:39 -0300

    Fix multiplication in multiplication table alert

--- questao4.js
+++ questao4.js
@@ -1,5 +1,5 @@
 let numero = Number(prompt("Digite um número para você ver a tabuada:"));
 
 for (let i = 1; i<= 10; i++){
-    alert(numero + " x " + i + " = " + (numero + i));
+    alert(numero + " x " + i + " = " + (numero * i));
 }
