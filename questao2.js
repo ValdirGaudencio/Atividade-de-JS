a1d03a5cd81db1ba7ae72dc991dbdec092d3bb52
Gaudencio <164903641+ValdirGaudencio@users.noreply.github.com>
2026-03-05 17:04:35 -0300

    Change alerts to console logs for number parity

--- questao2.js
+++ questao2.js
@@ -1,7 +1,7 @@
 let numero = Number(prompt("Digite um número:"));
 
-if (numero %2 ==0) {
-    alert("Numero Par");
-}  else {
-    alert("Número ímpar");
+if (numero % 2 === 0) {
+    console.log("O número " + numero + " é par!");
+} else {
+    console.log("O número " + numero + " é ímpar!");
 }
