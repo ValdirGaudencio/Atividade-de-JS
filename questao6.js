9f860fa6e09e66f8265daf0f6d39f4827076fd0b
Gaudencio <164903641+ValdirGaudencio@users.noreply.github.com>
2026-03-05 16:47:17 -0300

    Create questao6.js

---
+++ questao6.js
@@ -0,0 +1,9 @@
+let n = parseInt(prompt("Digite um número inteiro positivo:"));
+
+if (n > 0) {
+    for (let i = 1; i <= n; i++) {
+        console.log(i);
+    }
+} else {
+    console.log("Por favor, digite um número válido.");
+}
