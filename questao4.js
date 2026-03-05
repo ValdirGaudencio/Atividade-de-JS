bd5cd9e20925fbada8b17f1e5cdaa2d77b4887eb
Gaudencio <164903641+ValdirGaudencio@users.noreply.github.com>
2026-03-05 17:06:23 -0300

    Refactor tabuada logic to use console.log

--- questao4.js
+++ questao4.js
@@ -1,5 +1,6 @@
-let numero = Number(prompt("Digite um número para você ver a tabuada:"));
+let numTabuada = Number(prompt("Digite um número para ver a tabuada:"));
 
-for (let i = 1; i<= 10; i++){
-    alert(numero + " x " + i + " = " + (numero * i));
+for (let i = 1; i <= 10; i++) {
+    let linha = numTabuada + " x " + i + " = " + (numTabuada * i);
+    console.log(linha);
 }
