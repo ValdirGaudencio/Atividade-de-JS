65c9f5743098e89cc60b72c7a6efc931378d332b
Gaudencio <164903641+ValdirGaudencio@users.noreply.github.com>
2026-03-05 16:24:49 -0300

    Add grade calculation and alert messages

--- questao5.js
+++ questao5.js
@@ -1,1 +1,13 @@
+let nota1 = Number(prompt("Digite a primeira nota:"));
+let nota2 = Number(prompt("Digite a segunda nota:"));
+let nota3 = Number(prompt("Digite a terceira nota:"));
 
+let media = (nota1 + nota2 + nota3) / 3;
+
+if (media >= 7) {
+    alert("Aprovado");
+} else if (media >= 5 && media < 7) {
+    alert("Recuperação");
+} else {
+    alert("Reprovado");
+}
